e6d15be3283d73c7c4bc257b8d1a0b19ac2995b4
marven <2365702941@qq.com>
2021-02-27 23:55:03 +0800

    项目初始结构创建

---
+++ mail-coupon/src/test/java/com/pp/member/coupon/MailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pp.member.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mail-ware/src/test/java/com/pp/ware/ware/MailWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pp.ware.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailWareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
